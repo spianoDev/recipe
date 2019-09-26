1cc8e4c29998735f38cf1145aa3abe6040df7bbf
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-09-26 16:17:10 -0500

    populated seed data

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-const MyRecipe = require("../model/recipe.js");
+const Recipe = require("../model/recipe.js");
 
-router.get('/', (req, res) => {
-    MyRecipe.find({}).then(recipes => {
+router.get("/", (req, res) => {
+    Recipe.find({}).then(recipes => {
+        console.log(recipes);
         res.render("index", { recipes })
     })
 });
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "hbs");
+
 const recipesController = require("./controllers/recipes.js");
 app.use("/", recipesController);
 
-app.set("view engine", "hbs");
+app.get("/" , (req, res) => {
+    res.render("index", { title:req.params.title })
+})
+
 app.listen(5000, () => {console.log("app running on 5000")});
